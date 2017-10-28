c8d727e21f9a8e4801bea0e6f7bdc057e51dbcfb
crazycog <liam.schoepp@ucalgary.ca>
2017-10-28 12:01:59 -0600

    Add files via upload

---
+++ Appointed/Views/Popups.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HCI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+    public partial class DraggablePopup : Popup
+    {
+        public DraggablePopup()
+        {
+            var thumb = new Thumb
+            {
+                Width = 0,
+                Height = 0,
+            };
+
+            MouseDown += (sender, e) =>
+            {
+                thumb.RaiseEvent(e);
+            };
+
+            thumb.DragDelta += (sender, e) =>
+            {
+                HorizontalOffset += e.HorizontalChange;
+                VerticalOffset += e.VerticalChange;
+            };
+        }
+    }
+
+}
