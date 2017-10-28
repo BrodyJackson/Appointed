b5dd9745167ad552426673ac7d98b6b66e004c28
dabinNotDabin <dabin.lamming@outlook.com>
2017-10-28 11:58:34 -0600

    Nothing
    Need to push to grab more stuff from sidebar.

--- Appointed/Views/Dialogs/ModifyAppointmentDialog.xaml.cs
+++ Appointed/Views/Dialogs/ModifyAppointmentDialog.xaml.cs
@@ -10,7 +10,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Appointed.Views.Dialogs
@@ -18,7 +17,7 @@ namespace Appointed.Views.Dialogs
     /// <summary>
     /// Interaction logic for ModifyAppointmentDialog.xaml
     /// </summary>
-    public partial class ModifyAppointmentDialog : UserControl
+    public partial class ModifyAppointmentDialog : Window
     {
         public ModifyAppointmentDialog()
         {
