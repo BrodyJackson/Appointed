a836117670edcc3eff236dc4ef0d23c486447483
dabinNotDabin <dabin.lamming@outlook.com>
2017-11-22 00:33:23 -0700

    Keyboard Input
    Started implementation

--- Appointed/Home.xaml.cs
+++ Appointed/Home.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 using Appointed.Views;
 using Appointed.Views.Sidebar;
 using Appointed.Views.Dialogs;
+using Appointed.ViewModels;
 
 namespace Appointed
 {
@@ -28,9 +29,75 @@ namespace Appointed
         {
             InitializeComponent();
 
+
+            EventManager.RegisterClassHandler
+            (
+                typeof(Window),
+                Keyboard.KeyUpEvent,
+                new KeyEventHandler(Key_Up), true
+            );
+
+
+  
             ShowHomeSidebar();
 
         }
+
+        private void Key_Up(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+            {
+                MessageBox.Show("Delete Up!");
+            }
+            else if (e.Key == Key.Left)
+            {
+                if (Keyboard.IsKeyDown(Key.LeftShift))
+                    ShiftScheduleView(-7);
+                else if (Keyboard.IsKeyDown(Key.LeftCtrl))
+                    ShiftScheduleView(-3);
+                else
+                    ShiftScheduleView(-1);
+            }
+            else if (e.Key == Key.Right)
+            {
+                if (Keyboard.IsKeyDown(Key.LeftShift))
+                    ShiftScheduleView(7);
+                else if (Keyboard.IsKeyDown(Key.LeftCtrl))
+                    ShiftScheduleView(3);
+                else
+                    ShiftScheduleView(1);
+            }
+            //else if (e.Key == Key.LeftShift)
+            //{
+            //    ShiftScheduleView(-7);
+            //}
+            //else if (e.Key == Key.RightShift)
+            //{
+            //    ShiftScheduleView(7);
+            //}
+            else if (e.Key == Key.Up)
+            {
+            }
+            else if (e.Key == Key.Down)
+            {
+
+            }
+            else if (e.Key == Key.Enter || e.Key == Key.Return)
+            {
+                MessageBox.Show("Enter or Return Up!");
+            }
+        }
+
+
+   
+        void ShiftScheduleView(int amount)
+        {
+            DayInformationViewModel DIVM = (DayInformationViewModel)this.DataContext;
+
+            if (DIVM.ShiftView.CanExecute(null))
+                DIVM.ShiftView.Execute(amount);
+        }
+
 
         private void ShowHomeSidebar()
         {
